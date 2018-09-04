375dbd3707a6dd0babf5a68be634b86fe2ebb0b5
Johnathan <jbwierzbicki@gmail.com>
2018-09-04 00:09:36 -0400

    added invoice builder form

--- myScript.js
+++ myScript.js
@@ -1,12 +1,10 @@
 $(document).ready(function(){
-    var date_input=$('input[name="date"]'); //our date input has the name "date"
-    var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
-    date_input.datepicker({
-        format: 'mm/dd/yyyy',
-        container: container,
-        todayHighlight: true,
-        autoclose: true,
-    })
+  var date_input=$('input[name="date"]'); //our date input has the name "date"
+  var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+  date_input.datepicker({
+    format: 'mm/dd/yyyy',
+    container: container,
+    todayHighlight: true,
+    autoclose: true,
+  })
 })
-
- $('.input-group.date').datepicker({format: "dd.mm.yyyy"}); 
